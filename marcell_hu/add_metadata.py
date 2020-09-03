fdc3148beb687363e07d89fb44b198a3fd1eda52
Halász Dávid <david.peter.halasz@gmail.com>
2020-09-03 18:32:00 +0200

    Restructured some things in add_metadata.py

--- marcell_hu/add_metadata.py
+++ marcell_hu/add_metadata.py
@@ -50,91 +50,17 @@ class MMeta:
         :return: A generator yields the output line-by-line
         """
         self._sentence_count += 1
-        orig_sent = []
 
+        # Collecting and processing of global metadatas
         if self._sentence_count == 1:
-            # from here: collect and process global metadatas (per document)
-            """
-            Example output global metadata
-            # global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC MARCELL:NE MARCELL:NP MARCELL:IATE MARCELL:EUROVOC
-            # newdoc id = hu-hat_mk18188_16252018xi29korm
-            # date = 2018
-            # title = 1625/2018. (XI. 29.) Korm. határozat
-            # type = határozat
-            # entype = decree
-            # issuer = Korm.
-            # topic = garanciához kapcsolódó állami készfizető kezesség nyújtásáról
-            """
+            for global_metadata in self._get_global_metadatas(sen):
+                yield global_metadata
 
-            lemmas = []
-            is_topic = False
-            title_end = False
-            title = ""
-            topic = ""
-
-            for line in sen:
-                space = " " if line[9] == "_" else ""
-                orig_sent.append(line[1] + space)
-
-                if is_topic:
-                    if line[1].replace("*", "") != ".":
-                        topic += line[1] + space
-                    else:
-                        is_topic = False
-
-                elif not title_end:
-                    lemmas.append(line[2])
-                    if self._is_huntype(line[2]):
-                        self._doc_type = line[2]
-                        title += self._doc_type
-                        title_end = True
-                        is_topic = True
-                    else:
-                        title += line[1] + space
-
-            self._identifier = self._pat_whitespaces.sub('', '_'.join(title.split())).lower(). \
-                translate(str.maketrans(self._accent_dict))
-
-            for metadata in self._get_metadatas(topic, self._identifier, lemmas,
-                                                '# global.columns = ' + ' '.join(self._header).upper(), title.split()):
-                yield metadata
-
-        #     end of collecting and processing of global metadatas
-
-        else:
-            for line in sen:
-                space = " " if line[9] == "_" else ""
-                orig_sent.append(line[1] + space)
-
-        # from here: get metadatas per sentence
-        paragraph_num = 1
-        sentence = "".join(orig_sent)
-
-        if self._doc_type == "törvény" or self._doc_type == "rendelet":
-            paragraph = self._pat_paragraph.match(sentence)
-            par_id = ""
-            if self._sentence_count > 1 and paragraph:
-                paragraph_num = int(paragraph.group().split(".")[0])
-
-                if paragraph_num != 1:
-                    par_id = "# newpar id = " + self._identifier + '-p' + str(paragraph_num)
-            elif self._sentence_count == 1:
-                par_id = "# newpar id = " + self._identifier + '-p' + str(paragraph_num)
-
-            for metadata in ([par_id],
-                             ["# sent_id = " + self._identifier + "-s" + str(self._sentence_count) + '-p' + str(
-                                 paragraph_num)],
-                             ["# text = " + sentence]):
-                yield metadata
-        else:
-            for metadata in (["# sent_id = " + self._identifier + "-s" + str(self._sentence_count)],
-                             ["# text = " + sentence]):
-                yield metadata
-
-        # end of collect and process of metadatas per sentence
+        # Collecting and processing of metadatas per sentence
+        for metadatas_per_sentence in self._get_metadatas_per_sentence(sen):
+            yield metadatas_per_sentence
 
         yield from sen
-
 
     def prepare_fields(self, field_names):
         """
@@ -152,27 +78,103 @@ class MMeta:
     def _is_huntype(self, word):
         return word in self._doc_types_hun_eng.keys()
 
-    def _get_metadatas(self, topic, identifier, lemmas, columns, title):
-        eng_type = self._get_eng_type(lemmas[-1])
-        hun_type = lemmas[-1]
+    def _get_global_metadatas(self, sen):
+        """
+        Example output global metadata
+        # global.columns = ID FORM LEMMA UPOS XPOS FEATS HEAD DEPREL DEPS MISC MARCELL:NE MARCELL:NP MARCELL:IATE MARCELL:EUROVOC
+        # newdoc id = hu-hat_mk18188_16252018xi29korm
+        # date = 2018
+        # title = 1625/2018. (XI. 29.) Korm. határozat
+        # type = határozat
+        # entype = decree
+        # issuer = Korm.
+        # topic = garanciához kapcsolódó állami készfizető kezesség nyújtásáról
+        """
 
-        # issuer = 'parlament' if hun_type == 'törvény' else title[2]
-        # issuer = "# issuer = " + issuer
+        orig_sent = []
+        # lemmas = []
+        is_topic = False
+        title_end = False
+        title = ''
+        topic = ''
+        date = ''
 
-        if hun_type != 'törvény':
-            issuer = "# issuer = " + title[-2]
-        else:
-            issuer = "# issuer = parlament"
+        for i, line in enumerate(sen):
+            # Iterating over sentence to get the topic, title, documentum type, lemmas,
+            # for and the original sentence as text
 
-        title = "# title = " + " ".join(title)
-        newdoc_id = "# newdoc id = hu-" + identifier
-        date = "# date = " + lemmas[0].split("/")[-1].replace(".", "")
+            if i == 0:
+                date = line[2]
+
+            space = " " if line[9] == "_" else ""
+            orig_sent.append(line[1] + space)
+
+            if is_topic is True:
+                if line[1].replace("*", "") != ".":
+                    topic += line[1] + space
+                else:
+                    is_topic = False
+
+            elif title_end is False:
+                # lemmas.append(line[2])
+                if self._is_huntype(line[2]):
+                    self._doc_type = line[2]
+                    title += self._doc_type
+                    title_end = True
+                    is_topic = True
+                else:
+                    title += line[1] + space
+
+        self._identifier = self._pat_whitespaces.sub('', '_'.join(title.split())).lower(). \
+            translate(str.maketrans(self._accent_dict))
+
+        # From here: finalize global metadata values
+        columns = '# global.columns = ' + ' '.join(self._header).upper()
+        eng_type = self._get_eng_type(self._doc_type)
+        hun_type = self._doc_type
+
+        issuer = title.split()[-2] if hun_type != 'törvény' else 'parlament'
+        issuer = "# issuer = " + issuer
+
         hun_type = "# type = " + hun_type
         eng_type = "# entype = " + eng_type
 
-        metadatas = [[columns], [newdoc_id], [date], [title], [hun_type], [eng_type], [issuer]]
+        title = "# title = " + title
+        newdoc_id = "# newdoc id = hu-" + self._identifier
+        date = "# date = " + date.split("/")[-1].replace(".", "")  # lemmas[0].split("/")[-1].replace(".", "")
+
+        global_metadatas = [[columns], [newdoc_id], [date], [title], [hun_type], [eng_type], [issuer]]
 
         if topic != '':
-            metadatas.append(["# topic = " + topic])
+            global_metadatas.append(["# topic = " + topic])
 
-        return metadatas
+        return global_metadatas
+
+    def _get_metadatas_per_sentence(self, sen):
+        orig_sent = []
+        for line in sen:
+            space = " " if line[9] == "_" else ""
+            orig_sent.append(line[1] + space)
+
+        # from here: get metadatas per sentence
+        paragraph_num = 1
+        sentence = "".join(orig_sent)
+
+        if self._doc_type == "törvény" or self._doc_type == "rendelet":
+            paragraph = self._pat_paragraph.match(sentence)
+            par_id = ""
+            if self._sentence_count > 1 and paragraph:
+                paragraph_num = int(paragraph.group().split(".")[0])
+
+                if paragraph_num != 1:
+                    par_id = "# newpar id = " + self._identifier + '-p' + str(paragraph_num)
+            elif self._sentence_count == 1:
+                par_id = "# newpar id = " + self._identifier + '-p' + str(paragraph_num)
+
+            return ([par_id],
+                    ["# sent_id = " + self._identifier + "-s" + str(self._sentence_count) + '-p' + str(
+                        paragraph_num)],
+                    ["# text = " + sentence])
+        else:
+            return (["# sent_id = " + self._identifier + "-s" + str(self._sentence_count)],
+                    ["# text = " + sentence])
